61a3fde46c34bf6adf055abc5ce134453d70a211
Wilco Tubog <wilco.tubog@smprime.com>
2020-02-27 14:09:33 +0800

    chore: task completion

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,10 +2,19 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO: Steps 4 & 8 - Declare member variables here:
+    TextView mStoryTextView;
+    Button mTopButtonView;
+    Button mBottomButtonView;
+
+    int mStoryIndex = 1;
+    int mPathIndex = 0;
 
 
     @Override
@@ -13,18 +22,70 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mStoryTextView = (TextView) findViewById(R.id.storyTextView);
+        mTopButtonView = (Button) findViewById(R.id.buttonTop);
+        mBottomButtonView = (Button) findViewById(R.id.buttonBottom);
 
-        // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
+        mTopButtonView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                if(mStoryIndex == 1) {
+                    mPathIndex = 1;
+                }
 
+                if(mPathIndex == 1 && mStoryIndex == 1) {
+                    mStoryTextView.setText(R.string.T3_Story);
+                    mTopButtonView.setText(R.string.T3_Ans1);
+                    mBottomButtonView.setText(R.string.T3_Ans2);
+                } else if(mPathIndex == 1 && mStoryIndex == 2) {
+                    mStoryTextView.setText(R.string.T6_End);
+                    hideButtons();
+                } else if (mPathIndex == 2 && mStoryIndex == 2) {
+                    mStoryTextView.setText(R.string.T3_Story);
+                    mTopButtonView.setText(R.string.T3_Ans1);
+                    mBottomButtonView.setText(R.string.T3_Ans2);
+                } else if (mPathIndex == 2 && mStoryIndex == 3) {
+                    mStoryTextView.setText(R.string.T6_End);
+                    hideButtons();
+                }
 
-        // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
+                mStoryIndex += 1;
+            }
+        });
 
+        mBottomButtonView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mStoryIndex == 1) {
+                    mPathIndex = 2;
+                }
 
+                if(mPathIndex == 2 && mStoryIndex == 1) {
+                    mStoryTextView.setText(R.string.T2_Story);
+                    mTopButtonView.setText(R.string.T2_Ans1);
+                    mBottomButtonView.setText(R.string.T2_Ans2);
+                } else if(mPathIndex == 1 && mStoryIndex == 2) {
+                    mStoryTextView.setText(R.string.T5_End);
+                    hideButtons();
+                } else if(mPathIndex == 2 && mStoryIndex == 2) {
+                    mStoryTextView.setText(R.string.T4_End);
+                    hideButtons();
+                } else if (mPathIndex == 2 && mStoryIndex == 3) {
+                    mStoryTextView.setText(R.string.T5_End);
+                    hideButtons();
+                }
 
-
-        // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
-
+                mStoryIndex += 1;
+            }
+        });
 
     }
+
+    public void hideButtons() {
+        mTopButtonView.setVisibility(View.GONE);
+        mBottomButtonView.setVisibility(View.GONE);
+    }
+
+
 }
